bf9dd48eb5e6db19aa2fbd5204f991bc56aba7ce
Mark Price <Mark.Price@isilon.com>
2015-10-26 12:37:37 -0700

    Update git-create-conflict.sh

--- git-create-conflict.sh
+++ git-create-conflict.sh
@@ -13,6 +13,14 @@ for i in 0 1 ; do
 	if [ $i -eq 0 ]; then git tag commit1; fi
 done
 
+git checkout -b update
+touch FileC
+git add FileC
+for i in 0; do
+   echo "topic change $i" >> FileC
+   git commit -m "\"topic change $i"\" FileC
+done
+
 git checkout -b topic commit0 
 for i in 0; do
 	echo "topic change $i" >> FileA
