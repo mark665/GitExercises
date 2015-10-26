aa645c55e71162153f8dddea7cd120a7d0e4a5a0
Mark Price <mark.price@isilon.com>
2015-10-26 12:34:37 -0700

    Updates from class

--- git-create-conflict.sh
+++ git-create-conflict.sh
@@ -9,20 +9,11 @@ git add FileA
 for i in 0 1 ; do
 	echo "change $i" >> FileA
 	git commit -m "\"change $i"\" FileA
-	git commit -m "\"change $i"\" FileB
 	if [ $i -eq 0 ]; then git tag commit0; fi
 	if [ $i -eq 0 ]; then git tag commit1; fi
 done
 
-git checkout -b update
-touch FileC
-git add FileC
-for i in 0; do
-	echo "topic change $i" >> FileC
-	git commit -m "\"topic change $i"\" FileC
-done
-
-git checkout -b topic commit0
+git checkout -b topic commit0 
 for i in 0; do
 	echo "topic change $i" >> FileA
 	git commit -m "\"topic change $i"\" FileA
